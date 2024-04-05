c2f9cd6d0a9e2593ef6fe0f70a0987573fda7d9e
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-04-05 07:48:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (98.61%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,26 +1,18 @@
-#include <stack>
-#include <string>
-#include <cmath>
-
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char> stk;
-        
-        for (char c : s) {
-            if (!stk.empty() && abs(stk.top() - c) == 32) { // Check if the current character and the top of the stack are same letter but different cases
-                stk.pop(); // Pop the character from the stack
+        int n = s.size();
+        for (int i = 0; i < n - 1; ) {
+            if (abs(int(s[i]) - int(s[i + 1])) == 32) {
+                s.erase(i, 2);
+                if (i > 0) {
+                    i--;
+                }
+                n -= 2; 
             } else {
-                stk.push(c); // Push the character onto the stack
+                i++;
             }
         }
-        
-        string result = "";
-        while (!stk.empty()) {
-            result = stk.top() + result; // Construct the result string in reverse order
-            stk.pop();
-        }
-        
-        return result;
+        return s;
     }
 };
