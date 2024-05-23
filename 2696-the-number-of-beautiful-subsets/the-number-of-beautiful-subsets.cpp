bd368597119a59988c5b3dd7f8e764a1f46ff366
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-05-23 08:52:35 +0530

    Time: 242 ms (61.98%) | Memory: 36 MB (87.60%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isvalid(int x, vector<int> &ds, int k)
+{
+    for (int i = 0; i < ds.size(); i++)
+    {
+        if (abs(ds[i] - x) == k)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve(int ind, vector<int> &ds, vector<int> &nums, int &ans, int n, int k)
+{
+    if (ind == n)
+    {
+        ans++;
+        return;
+    }
+    if (isvalid(nums[ind], ds, k))
+    {
+        ds.push_back(nums[ind]);
+        solve(ind + 1, ds, nums, ans, n, k);
+        ds.pop_back();   
+    }
+    solve(ind + 1, ds, nums, ans, n, k);
+}
+
+int beautifulSubsets(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    vector<int> ds;
+    int ans = 0;
+    solve(0, ds, nums, ans, n, k);
+    return ans - 1;
+}
+};
