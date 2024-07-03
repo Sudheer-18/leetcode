b5c61aa311e71441655546f938df305b6112bda8
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-07-03 16:00:10 +0530

    Time: 4 ms (62.21%) | Memory: 12.6 MB (82.69%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -6,9 +6,8 @@ public:
             return;
         }
         if (sum > tar || ind >= size) return;
-        
-        for (int i = ind; i < size; ++i) {
-            if (i > ind && nums[i] == nums[i-1]) continue;  // Skip duplicates
+        for (int i = ind; i < size; i++) {
+            if (i > ind && nums[i] == nums[i-1]) continue;  
             v1.push_back(nums[i]);
             recursion(nums, tar, size, i + 1, sum + nums[i], ans, v1);
             v1.pop_back();
@@ -18,7 +17,7 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int tar) {
         vector<vector<int>> ans;
         vector<int> res;
-        sort(nums.begin(), nums.end());  // Sort the input to handle duplicates
+        sort(nums.begin(), nums.end());  
         recursion(nums, tar, nums.size(), 0, 0, ans, res);
         return ans;
     }
