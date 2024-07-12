bf0eeb2bda8765b88d5c0f6470fa7a22b71ad3ec
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-07-12 17:56:13 +0530

    Time: 266 ms (61.69%) | Memory: 18.6 MB (20.78%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -6,21 +6,24 @@ class Solution:
         else:
             pattern1, pattern2 = 'ab', 'ba'
         
-        def remove_pattern_and_score(s, pattern, score):
-            stack = []
-            points = 0
-            for char in s:
-                if stack and stack[-1] == pattern[0] and char == pattern[1]:
-                    stack.pop()
-                    points += score
-                else:
-                    stack.append(char)
-            return ''.join(stack), points
+        points = 0
+        stack = []
         
-        # First, remove and score the more valuable pattern
-        remaining_string, points = remove_pattern_and_score(s, pattern1, x)
+        # Process pattern1 ('ba') first
+        for char in s:
+            if stack and stack[-1] == pattern1[0] and char == pattern1[1]:
+                stack.pop()
+                points += x
+            else:
+                stack.append(char)
         
-        # Then, remove and score the less valuable pattern
-        _, additional_points = remove_pattern_and_score(remaining_string, pattern2, y)
+        # Process pattern2 ('ab') second
+        temp_stack = []
+        for char in stack:
+            if temp_stack and temp_stack[-1] == pattern2[0] and char == pattern2[1]:
+                temp_stack.pop()
+                points += y
+            else:
+                temp_stack.append(char)
         
-        return points + additional_points
+        return points
