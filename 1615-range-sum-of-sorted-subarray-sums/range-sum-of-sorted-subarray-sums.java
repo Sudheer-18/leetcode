a0b5d45d2ba024931fb9eccac542c6a1f7ef0f2d
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-08-04 07:52:09 +0530

    Time: 66 ms (68.47%) | Memory: 46.9 MB (39.19%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int i = 0, j = 0, k = 0;
+        int size = (n * (n + 1)) / 2;
+        int[] arr = new int[size];
+        int pre_sum = 0;
+        
+        while (i < n) {
+            if (j == n) {
+                i += 1;
+                j = i;
+                pre_sum = 0;
+                if (i >= n) break;
+            }
+            pre_sum += nums[j];
+            arr[k] = pre_sum;
+            j += 1;
+            k += 1;
+        }
+        
+        Arrays.sort(arr);
+        int ans = 0;
+        for (i = left - 1; i < right; i++) {
+            ans = (ans + arr[i]) % 1000000007;
+        } 
+        return ans;
+    }
+}
