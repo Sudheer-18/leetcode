2182b9bcc8c9d55eb0800cbbb00e35ac31a68ecb
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-08-13 23:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (32.08%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    int tribonacci(int n) {
-        std::unordered_map<int, int> memo;
-        return helper(n, memo);
+    int tb(int n ,vector<int>& dp){
+        if(n==0) return 0;
+        if(n==1 || n ==2) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = tb(n-1,dp)+tb(n-2,dp)+tb(n-3,dp);
     }
-    
-private:
-    int helper(int n, std::unordered_map<int, int>& memo) {
-        if (memo.find(n) != memo.end()) {
-            return memo[n];
-        }
-        
-        int result;
-        if (n == 0) {
-            result = 0;
-        } else if (n == 1 || n == 2) {
-            result = 1;
-        } else {
-            result = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
-        }
-        
-        memo[n] = result;
-        return result;
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 || n ==2) return 1;
+        vector<int> dp(n+1,-1);
+        tb(n,dp);
+        return dp[n];
     }
 };
