e126d9eca527fcb0285225279468bc9a51312330
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-09-18 14:41:14 +0530

    Time: 6 ms (67.11%) | Memory: 17.4 MB (14.46%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -5,11 +5,11 @@ public:
         for(int n : nums) {
             numStrs.push_back(to_string(n));
         }
-        sort(numStrs.begin(), numStrs.end(), [](string &a, string &b) {
+        sort(numStrs.begin(), numStrs.end(), [](string a, string b) {
             return a + b > b + a; 
         });
         string res;
-        for(string &s : numStrs) {
+        for(string s : numStrs) {
             res += s;
         }
         return res[0] == '0' ? "0" : res;
