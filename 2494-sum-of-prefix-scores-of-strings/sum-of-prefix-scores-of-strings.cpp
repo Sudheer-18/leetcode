b3b548a95e58e85eac8f1864dc6040d8f3510770
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-09-25 22:09:41 +0530

    Time: 619 ms (40.15%) | Memory: 711.9 MB (50.87%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,50 @@
+
+class TrieNode {
+public:
+    TrieNode* children[26] = {nullptr}; 
+    int prefixCount = 0; 
+};
+class Trie {
+private:
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            if (!node->children[ch - 'a']) {
+                node->children[ch - 'a'] = new TrieNode();
+            }
+            node = node->children[ch - 'a'];
+            node->prefixCount++; 
+        }
+    }
+    int getPrefixScore(const string& word) {
+        TrieNode* node = root;
+        int score = 0;
+        for (char ch : word) {
+            node = node->children[ch - 'a'];
+            score += node->prefixCount;
+        }
+        return score;
+    }
+};
+
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie trie;
+        for (string word : words) {
+            trie.insert(word);
+        }
+        vector<int> result;
+        for (const string& word : words) {
+            result.push_back(trie.getPrefixScore(word));
+        }
+        
+        return result;
+    }
+};
+
