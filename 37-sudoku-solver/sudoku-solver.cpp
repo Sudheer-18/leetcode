8875ab36389721f1c666a5f2c2c40b504b7f3775
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-09-27 22:55:01 +0530

    Time: 11 ms (72.75%) | Memory: 8 MB (75.59%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -32,43 +32,5 @@ public:
         }
         return true;
     }
-    bool is_valid(int s, int e, vector<vector<char>>& board) {
-        map<char, int> mp;
-        for (int i = s; i < s + 3; i++) {
-            for (int j = e; j < e + 3; j++) {
-                if (board[i][j] == '.') continue;
-                if (mp.find(board[i][j]) != mp.end()) return false;
-                else mp[board[i][j]]++;
-            }
-        }
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size(); 
-        int m = board[0].size(); 
-        map<char, int> mp;
-        for (int i = 0; i < n; i++) {
-            mp.clear();
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == '.') continue;
-                if (mp.find(board[i][j]) != mp.end()) return false;
-                else mp[board[i][j]]++;
-            }
-        }
-        for (int j = 0; j < m; j++) {
-            mp.clear();
-            for (int i = 0; i < n; i++) {
-                if (board[i][j] == '.') continue;
-                if (mp.find(board[i][j]) != mp.end()) return false;
-                else mp[board[i][j]]++;
-            }
-        }
-        for (int i = 0; i < n; i += 3) {
-            for (int j = 0; j < m; j += 3) {
-                if (!is_valid(i, j, board)) return false;
-            }
-        }
-
-        return true;
-    }
+    
 };
