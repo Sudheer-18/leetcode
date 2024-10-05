c2fb7a4f321066454d6e46a26759be7774a908a9
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2024-10-05 07:09:28 +0530

    Time: 10 ms (41.85%) | Memory: 8.7 MB (92.55%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -5,12 +5,12 @@ public:
         int n2 = s2.size();
         if (n1 > n2) return false;  
         vector<int> sf1(26, 0), sf2(26, 0);
-        for (int i = 0; i < n1; ++i) {
+        for (int i = 0; i < n1; i++) {
             sf1[s1[i] - 'a']++;
             sf2[s2[i] - 'a']++;
         }
         if (sf1 == sf2) return true;
-        for (int i = n1; i < n2; ++i) {
+        for (int i = n1; i < n2; i++) {
             sf2[s2[i] - 'a']++;
             sf2[s2[i - n1] - 'a']--;
             if (sf1 == sf2) return true;
