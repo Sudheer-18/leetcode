66b4deb4297850da25c9701f539d851921b701a0
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-01-23 23:57:54 +0530

    Time: 22 ms (81.58%) | Memory: 109.1 MB (69.88%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -3,7 +3,7 @@ public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
         vector<long long> pre(n+1, 0);
-        // pre[0] = nums[0];
+        pre[0] = nums[0];
         for (int i=1; i<=n; i++) pre[i] = pre[i-1]+nums[i-1];
         deque<int> dq;
         int ans = n+1;
