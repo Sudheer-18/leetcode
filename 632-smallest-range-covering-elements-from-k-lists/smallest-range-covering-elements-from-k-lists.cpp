9fab8d52e635b061d2c9a8dfb30b45336c676479
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-01-25 21:13:04 +0530

    Time: 135 ms (26.89%) | Memory: 69.8 MB (19.32%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        multimap<int, pair<int, int>> mp;
+        int k = nums.size();
+        int maxVal = INT_MIN;
+        for (int i = 0; i < k; i++) {
+            mp.insert({nums[i][0], {i, 0}});
+            maxVal = max(maxVal, nums[i][0]);
+        }
+
+        int rs = 0, re = INT_MAX;
+        while (true) {
+            auto it = mp.begin();
+            int minVal = it->first;
+            int li = it->second.first;
+            int ind = it->second.second;
+            if (maxVal - minVal < re - rs || 
+                (maxVal - minVal == re - rs && minVal < rs)) {
+                rs = minVal;
+                re = maxVal;
+            }
+            mp.erase(it);
+            if (ind + 1 < nums[li].size()) {
+                int val = nums[li][ind + 1];
+                mp.insert({val, {li, ind + 1}});
+                maxVal = max(maxVal, val);
+            } 
+            else {
+                break;
+            }
+        }
+
+        return {rs, re};
+    }
+};
