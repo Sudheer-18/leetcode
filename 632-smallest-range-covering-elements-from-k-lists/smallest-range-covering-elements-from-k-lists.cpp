ee98e34fb1fda843fdc36cd97f665ba7ff827010
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-01-25 23:18:30 +0530

    Time: 130 ms (27.91%) | Memory: 69.9 MB (18.64%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -4,12 +4,14 @@ public:
         multimap<int, pair<int, int>> mp;
         int k = nums.size();
         int maxVal = INT_MIN;
+        long long maxi = INT_MIN;
         for (int i = 0; i < k; i++) {
             mp.insert({nums[i][0], {i, 0}});
             maxVal = max(maxVal, nums[i][0]);
+            if(maxi < nums[i].size()) maxi = nums[i].size();
         }
         int rs = 0, re = INT_MAX;
-        while (true) {
+        while (maxi--) {
             auto it = mp.begin();
             int minVal = it->first;
             int li = it->second.first;
@@ -28,7 +30,6 @@ public:
                 break;
             }
         }
-
         return {rs, re};
     }
 };
