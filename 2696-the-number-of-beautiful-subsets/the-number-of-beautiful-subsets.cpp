900c587c1410a587b136bdbfd72d973f571810b2
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-01-30 22:05:20 +0530

    Time: 231 ms (79.06%) | Memory: 37.5 MB (86.99%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    bool isvalid(int x, vector<int> &ds, int k)
-{
-    for (int i = 0; i < ds.size(); i++)
-    {
-        if (abs(ds[i] - x) == k)
-        {
-            return false;
+    bool isvalid(int x, vector<int> &ds, int k) {
+        for (int i = 0; i < ds.size(); i++) {
+            if (abs(ds[i] - x) == k) return false;
         }
-    }
-    return true;
+        return true;
 }
 
-void solve(int ind, vector<int> &ds, vector<int> &nums, int &ans, int n, int k)
-{
-    if (ind == n)
-    {
+void solve(int ind, vector<int> &ds, vector<int> &nums, int &ans, int n, int k) {
+    if (ind == n) {
         ans++;
         return;
     }
-    if (isvalid(nums[ind], ds, k))
-    {
+    if (isvalid(nums[ind], ds, k)) {
         ds.push_back(nums[ind]);
         solve(ind + 1, ds, nums, ans, n, k);
         ds.pop_back();   
@@ -28,12 +20,11 @@ void solve(int ind, vector<int> &ds, vector<int> &nums, int &ans, int n, int k)
     solve(ind + 1, ds, nums, ans, n, k);
 }
 
-int beautifulSubsets(vector<int> &nums, int k)
-{
+int beautifulSubsets(vector<int> &nums, int k) {
     int n = nums.size();
     vector<int> ds;
     int ans = 0;
     solve(0, ds, nums, ans, n, k);
     return ans - 1;
-}
+    }
 };
