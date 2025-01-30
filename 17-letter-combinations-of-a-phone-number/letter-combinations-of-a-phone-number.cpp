5f12b6de74a9f4f9ffe31622b3541a934d3e814a
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-01-30 23:04:18 +0530

    Time: 1 ms (21.11%) | Memory: 9.7 MB (11.47%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<string> ans;
-    void fun(map<char,string>& mp, int i, string s, string dig, int n) {
+    void fun(unordered_map<char,string>& mp, int i, string s, string dig, int n) {
         if(i >= n) {
             ans.push_back(s);
             return;
         }
         string temp = mp[dig[i]];
-        for(int j = 0; j < temp.length(); j++) {
-            fun(mp, i + 1, s + temp[j], dig, n);
+        for(char ch : temp) {
+            fun(mp, i + 1, s + ch, dig, n);
         }
     }
     
     vector<string> letterCombinations(string digits) {
-        map<char,string> mp;
+        unordered_map<char,string> mp;
         mp['2'] = "abc";
         mp['3'] = "def";
         mp['4'] = "ghi";
