303ef07b591b6583ca06f6d437ed2901e2cf5e05
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-02-06 18:29:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (14.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    void recursion(vector<vector<int>>& arr, vector<int>& ans, int m, int n, int i, int j, int dir, vector<vector<bool>>& visited) {
-        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j]) return;
-        visited[i][j] = true;
+    vector<int> ans;
+    void spiral_matrix(vector<vector<int>>& arr,int i,int j,int n, int m ,int dir) {
+        if (i < 0 || i >= n || j < 0 || j >= m || arr[i][j] == -1000) return;
         ans.push_back(arr[i][j]);
+        arr[i][j] = -1000;
         if (dir == 0) { 
-            if (j + 1 < n && !visited[i][j + 1]) 
-                recursion(arr, ans, m, n, i, j + 1, 0, visited);
-            else 
-                recursion(arr, ans, m, n, i + 1, j, 1, visited);  
+            if (j + 1 < m && arr[i][j + 1] != -1000)
+                spiral_matrix(arr, i, j + 1, n, m, 0);
+            else
+                spiral_matrix(arr, i + 1, j, n, m, 1);
         } 
-        else if (dir == 1) {  
-            if (i + 1 < m && !visited[i + 1][j]) 
-                recursion(arr, ans, m, n, i + 1, j, 1, visited);
-            else 
-                recursion(arr, ans, m, n, i, j - 1, 2, visited);  
+        else if (dir == 1) { 
+            if (i + 1 < n && arr[i + 1][j] != -1000)
+                spiral_matrix(arr, i + 1, j, n, m, 1);
+            else
+                spiral_matrix(arr, i, j - 1, n, m, 2);
         } 
-        else if (dir == 2) { 
-            if (j - 1 >= 0 && !visited[i][j - 1]) 
-                recursion(arr, ans, m, n, i, j - 1, 2, visited);
-            else 
-                recursion(arr, ans, m, n, i - 1, j, 3, visited); 
+        else if (dir == 2) {
+            if (j - 1 >= 0 && arr[i][j - 1] != -1000)
+                spiral_matrix(arr, i, j - 1, n, m, 2);
+            else
+                spiral_matrix(arr, i - 1, j, n, m, 3);
         } 
         else if (dir == 3) { 
-            if (i - 1 >= 0 && !visited[i - 1][j]) 
-                recursion(arr, ans, m, n, i - 1, j, 3, visited);
-            else 
-                recursion(arr, ans, m, n, i, j + 1, 0, visited);  
+            if (i - 1 >= 0 && arr[i - 1][j] != -1000)
+                spiral_matrix(arr, i - 1, j, n, m, 3);
+            else
+                spiral_matrix(arr, i, j + 1, n, m, 0);
         }
+
     }
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();         
-        int n = matrix[0].size();      
-        vector<int> res;              
-        vector<vector<bool>> visited(m, vector<bool>(n, false)); 
-        recursion(matrix, res, m, n, 0, 0, 0, visited);
-        return res;
+        spiral_matrix(matrix,0,0,matrix.size(),matrix[0].size(), 0);
+        return ans;
     }
 };
