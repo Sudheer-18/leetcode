d46604c2c0e15bf2c02d41a43926a61cac1fcb32
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-02-27 23:40:08 +0530

    Time: 2 ms (58.46%) | Memory: 8.5 MB (42.42%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int ans = 0;
+    bool Valid(vector<string>& grid,int i,int j) {
+        for(int c = j + 1; c < grid.size(); c++) {
+            if(grid[i][c] == 'Q') return false;
+        }
+        for(int r = i + 1, c =j + 1 ; r < grid.size() && c < grid.size(); r++ , c++) {
+            if(grid[r][c] == 'Q') return false;
+        }
+        for(int r = i - 1 , c = j + 1; r >= 0 && c < grid.size() ; r--, c++) {
+            if(grid[r][c] == 'Q') return false;
+        }
+        return true;
+    }
+    void helper(int n , vector<string>& grid) {
+        if(n < 0) {
+            ans++;
+            return;
+        }
+        for(int i = 0; i < grid.size(); i++) {
+            if(Valid(grid,i,n)) {
+                char ch = 'Q';
+                swap(grid[i][n],ch);
+                helper(n-1,grid);
+                swap(grid[i][n],ch);
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string> grid(n,string(n,'.'));
+        helper(n-1,grid);
+        return ans;
+    }
+};
