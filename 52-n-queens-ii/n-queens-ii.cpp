a1de3cb903ce79757bbaf5f80e844b29ae6249ab
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-03-05 22:35:34 +0530

    Time: 2 ms (58.15%) | Memory: 8.3 MB (53.30%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int ans = 0;
-    bool Valid(vector<string>& grid,int i,int j) {
+     bool Valid(vector<string>& grid,int i ,int j) {
         for(int c = j + 1; c < grid.size(); c++) {
             if(grid[i][c] == 'Q') return false;
         }
-        for(int r = i + 1, c =j + 1 ; r < grid.size() && c < grid.size(); r++ , c++) {
+        for(int r = i + 1 , c = j + 1; r < grid.size() && c < grid.size(); r++, c++) {
             if(grid[r][c] == 'Q') return false;
         }
-        for(int r = i - 1 , c = j + 1; r >= 0 && c < grid.size() ; r--, c++) {
+        for(int r = i - 1 , c = j + 1; r >= 0 && c < grid.size(); r--, c++) {
             if(grid[r][c] == 'Q') return false;
         }
+
         return true;
-    }
-    void helper(int n , vector<string>& grid) {
+     }
+     void helper(int n, vector<string>& grid) {
         if(n < 0) {
-            ans++;
+           ans++;
             return;
         }
         for(int i = 0; i < grid.size(); i++) {
             if(Valid(grid,i,n)) {
-                char ch = 'Q';
-                swap(grid[i][n],ch);
-                helper(n-1,grid);
-                swap(grid[i][n],ch);
+                 char ch = 'Q';
+                 swap(grid[i][n] , ch) ;
+                 helper(n - 1,grid);
+                 swap(grid[i][n] , ch) ;
             }
         }
-    }
+     }
     int totalNQueens(int n) {
         vector<string> grid(n,string(n,'.'));
-        helper(n-1,grid);
+        helper(n - 1,grid);
         return ans;
     }
 };
