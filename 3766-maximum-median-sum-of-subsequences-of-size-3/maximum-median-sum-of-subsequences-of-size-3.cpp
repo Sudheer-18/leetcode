e1ac13c806c8ca763642445708b36d4e0898716d
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-07-27 16:52:19 +0530

    Time: 301 ms (5.04%) | Memory: 285.1 MB (5.50%) - LeetSync

--- 3766-maximum-median-sum-of-subsequences-of-size-3/maximum-median-sum-of-subsequences-of-size-3.cpp
+++ 3766-maximum-median-sum-of-subsequences-of-size-3/maximum-median-sum-of-subsequences-of-size-3.cpp
@@ -6,15 +6,16 @@ public:
         sort(begin(nums),end(nums));
         int i = 0, j = n - 1;
         while(i < j) {
-            vector<int> V;
-            V.push_back(nums[i]);
-            while(j >= 0 && V.size() != 3) {
-                V.push_back(nums[j]);
+            multiset<int> ms;
+            ms.insert(nums[i]);
+            while(j >= 0 && ms.size() != 3) {
+                ms.insert(nums[j]);
                 j--;
             }
-            sort(begin(V),end(V));
-            ans += V[1];
-            V.clear();
+            auto it = ms.begin();
+            advance(it,ms.size()/2);
+            ans += *it;
+            ms.clear();
             i++;
         }
 
