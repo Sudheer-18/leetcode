c8b969ec82e307b57a846f4ce1c921d0f3e4a069
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-08-31 11:16:53 +0530

    Time: 2 ms (77.42%) | Memory: 44.8 MB (39.45%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,44 +1,40 @@
 class Solution {
-    private static boolean check(int s, int e, char[][] board) {
-        HashSet<Character> hs = new HashSet<>();
-        for (int i = s; i < s + 3; i++) {
-            for (int j = e; j < e + 3; j++) {
-                char ch = board[i][j];
-                if (ch == '.') continue;
-                if (hs.contains(ch)) return false;
-                hs.add(ch);
-            }
-        }
-        return true; 
-    }
-
     public boolean isValidSudoku(char[][] board) {
-        int R = board.length;
-        int C = board[0].length;
-        for (int i = 0; i < R; i++) {
+        for(int i=0; i<9; i++) {
             HashSet<Character> hs = new HashSet<>();
-            for (int j = 0; j < C; j++) {
+            for(int j=0; j<9; j++) {
                 char ch = board[i][j];
-                if (ch == '.') continue;
-                if (hs.contains(ch)) return false;
+                if(ch == '.') continue;
+                if(hs.contains(ch)) return false;
                 hs.add(ch);
             }
         }
-        for (int j = 0; j < C; j++) {
+        for(int i=0; i<9; i++) {
             HashSet<Character> hs = new HashSet<>();
-            for (int i = 0; i < R; i++) {
-                char ch = board[i][j];
-                if (ch == '.') continue;
-                if (hs.contains(ch)) return false;
+            for(int j=0; j<9; j++) {
+                char ch = board[j][i];
+                if(ch == '.') continue;
+                if(hs.contains(ch)) return false;
                 hs.add(ch);
             }
         }
-        for (int i = 0; i < R; i += 3) {
-            for (int j = 0; j < C; j += 3) {
-                if (!check(i, j, board)) return false;
+        for(int i=0; i<9; i+=3) {
+            for(int j=0; j<9; j+=3) {
+                if(!check(board,i,j)) return false;
             }
         }
-
+        return true;
+    }
+    public static boolean check(char[][] board, int r ,int c) {
+        HashSet<Character> hs = new HashSet<>();
+        for(int i=r; i<r+3; i++) {
+            for(int j=c; j<c+3;j++) {
+                char ch = board[i][j];
+                if(ch == '.') continue;
+                if(hs.contains(ch)) return false;
+                hs.add(ch);
+            }
+        }
         return true;
     }
 }
