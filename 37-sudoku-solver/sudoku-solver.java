70e8edbced91b6be280f0bbf47c7fe421456f4ec
k.sudheer <119719131+Sudheer-18@users.noreply.github.com>
2025-08-31 10:58:05 +0530

    Time: 206 ms (61.99%) | Memory: 41.2 MB (56.80%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,23 +1,13 @@
 class Solution {
-    public static boolean validcell(char[][] board,int r, int c, char ch) {
-        for(int i=0; i<9; i++) {
-            if(board[r][i] == ch || board[i][c] == ch) return false;
-        }
-        r -= r % 3;
-        c -= c % 3;
-        for(int i=r; i < r+3;i++) {
-            for(int j=c; j<c+3; j++) {
-                if(board[i][j] == ch) return false;
-            }
-        }
-        return true;
+    public void solveSudoku(char[][] board) {
+        solve(board);
     }
     public static boolean solve(char[][] board) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
                 if(board[i][j] == '.') {
-                    for(char ch = '1'; ch <= '9'; ch++) {
-                        if(validcell(board,i,j,ch)) {
+                    for(char ch='1'; ch<='9'; ch++) {
+                        if(checkvalid(board,i,j,ch)) {
                             board[i][j] = ch;
                             if(solve(board)) return true;
                             board[i][j] = '.';
@@ -29,7 +19,17 @@ class Solution {
         }
         return true;
     }
-    public void solveSudoku(char[][] board) {
-        solve(board);
+    public static boolean checkvalid(char[][] board, int r,int c,char ch) {
+        for(int i=0; i<9; i++) {
+            if(board[r][i] == ch || board[i][c] == ch) return false;  
+        }
+        r -= r%3;
+        c -= c%3;
+        for(int i=r; i<r+3; i++) {
+            for(int j=c; j<c+3; j++) {
+                if(board[i][j] == ch) return false;
+            }
+        }
+        return true;
     }
 }
